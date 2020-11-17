4bd30b6b2f5cb0f9b6632918627089a00831525b
ReformedMrozu <p.mroz20@outlook.com>
2020-11-17 22:57:02 +0100

    added player gravity using computedStyle

--- scripts/gameController.js
+++ scripts/gameController.js
@@ -5,3 +5,11 @@ crossing.addEventListener('animationiteration', () => {
     let randomGenerator = (Math.random() * 400 + 200)
     crossing.style.top = -randomGenerator + "px"
 })
+
+setInterval(() => {
+
+    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
+    if (playerTop <= 550) {
+        player.style.top = (playerTop + 3)
+    }
+}, 16.6)
